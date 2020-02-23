2911fdf8dac8f0273015989e2e9b504d8c64f2ac
inhoinno <56788537+inhoinno@users.noreply.github.com>
2020-02-23 14:39:27 +0900

    Date 23 Feb 2020, code for LPI ch22 ex1 about SIGCONT , by mearrong123@gmail.com

---
+++ sigcont.c
@@ -0,0 +1,55 @@
+/*
+
+Section 22.2 noted that if a stopped process that has established a handler for and
+22.2 장에서 sigcont 에 대한 핸들러를 설정하고 sigcont를 블록한 프로세스에 대해서
+
+blocked SIGCONT is later resumed as a consequence of receiving a SIGCONT, then the
+프로세스가 다시 시작되었을 때 (sigcont 를 받아서), 그때 핸들러가 실행된다는 것을 보여라(sigcont를 블록 했을 때 만) 
+handler is invoked only when SIGCONT is unblocked. Write a program to verify this.
+
+Recall that a process can be stopped by typing the terminal suspend character
+(usually Control-Z) and can be sent a SIGCONT signal using the command kill –CONT
+(or implicitly, using the shell fg command).
+
+*/
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+void 
+continue_handler(int sig) 
+{
+    //handler for sigcont 
+    printf("**** Hi, %s Handler invoked ... ****\n", strsignal(sig));
+
+}
+int main()
+{
+    //1 set a handler for sigcont
+    //2 block sigcont . use sigprocmask
+    //3 suspend process(by Cntrl z)
+    //4 in terminal, type kill -SIGCONT pid
+    //5 see
+
+    // sigaction(int sig , struct sigaction *new, struct sigaction *old)
+    int i=0;
+    int j=0;
+
+    struct sigaction newact,oldact;
+    sigemptyset(&newact.sa_mask);
+    &newact.sa_flags = 0;
+    &newact.sa_handler = continue_handler;
+    sigaddset(&newact.sa_mask, SIGCONT);
+    if(sigaction(SIGCONT, &newact, &oldact) <0){
+        perror("sigaction"); exit(-1);
+    }
+    printf("Process %d get started ...\n", getpid());
+    while(1){
+        printf("%d\n", i++);
+        sleep(2);
+    }
+
+    printf("PID %d Exit Successfully..\n", getpid());
+    exit(1);
+
+}
